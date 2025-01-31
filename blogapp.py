06db3458c14f29a37883df44c867a975c826aa4c
Benson-Gikonyo <muhiugikonyo@gmail.com>
2025-02-01 00:31:26 +0300

    initial commit

---
+++ blogapp.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def get_info():
+
+    email = "muhiugikonyo@gmail.com"
+
+    current_datetime = datetime.now(pytz.utc).isoformat()
+ 
+    github_url = "https://github.com/Benson-Gikonyo/HNG-api"
+
+    # JSON response
+    response = {
+        "email": email,
+        "current_datetime": current_datetime,
+        "github_url": github_url
+    }
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    # Enable CORS
+    from flask_cors import CORS
+    CORS(app)
+
+    app.run(debug=True, host='0.0.0.0', port=5000)
